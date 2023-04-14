28b667c0aad5d090bf402fd3f56842092531dce7
ahaddad98 <amine.haddadd@gmail.com>
2023-04-14 16:00:17 +0000

    cylinders not showing correctly

--- screen/ViewerScreen.js
+++ screen/ViewerScreen.js
@@ -14,17 +14,9 @@ import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 import { StatusBar } from "expo-status-bar";
 import { parsePdbFunction } from "../components/parsePdb";
 import styled from "styled-components";
+import { CylinderGeometry } from "three";
 
 const ViewerScreen = () => {
-  // const [data, setData] = useState();
-  // useEffect(()=>{
-  //     console.log('===========================================================================');
-  //     console.log(dataparsed);
-  //     console.log('===========================================================================');
-  //     setData(dataparsed)
-  //   }
-  //   )
-  // },[])
   const onContextCreate = async (gl) => {
     // three.js implementation.
     parsePdbFunction('011').then((data) => {
@@ -39,83 +31,66 @@ const ViewerScreen = () => {
         width: gl.drawingBufferWidth,
         height: gl.drawingBufferHeight,
       };
-
+  
       // set camera position away from sphere
       camera.position.z = 5;
-
+  
       const renderer = new Renderer({ gl });
       // set size of buffer to be equal to drawing buffer width
       renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
-
+  
       // create render function
       const render = () => {
         requestAnimationFrame(render);
         renderer.render(scene, camera);
         gl.endFrameEXP();
       };
-
+  
       if (data) {
         // create atom material
         const atomMaterial = new MeshBasicMaterial({
           color: "red",
         });
-
+  
         // create bond material
         const bondMaterial = new MeshBasicMaterial({
-          color: "grey",
+          color: "green",
         });
-
-        // create atoms and bonds
-        const atoms = [];
-        const bonds = [];
-        data.atoms.forEach((atom) => {
-          const geometry = new SphereGeometry(0.2, 32, 32);
-          const mesh = new Mesh(geometry, atomMaterial);
-          mesh.position.set(atom.x, atom.y, atom.z);
-          scene.add(mesh);
-          atoms.push(mesh);
+  
+        // loop through all the atoms in the data
+        // data.atoms.forEach((data) => {
+          // create atoms and bonds for the current molecule
+          const atoms = [];
+          const bonds = [];
+          data.atoms.forEach((atom) => {
+            const geometry = new SphereGeometry(0.2, 32, 32);
+            const mesh = new Mesh(geometry, atomMaterial);
+            mesh.position.set(atom.x, atom.y, atom.z);
+            scene.add(mesh);
+            atoms.push(mesh);
+          });
+          data.connectData.forEach((bond) => {
+            const startAtom = data.serials[bond[0]];
+            const endAtom = data.serials[bond[1]];
+            const distance = Math.sqrt((endAtom.x - startAtom.x) ** 2 + (endAtom.y - startAtom.y) ** 2 + (endAtom.z - startAtom.z) ** 2);
+            const geometry = new CylinderGeometry(0.1, 0.1, distance, 32);
+            const mesh = new Mesh(geometry, bondMaterial);
+  
+            mesh.position.set(
+              (startAtom.x + endAtom.x) / 2,
+              (startAtom.y + endAtom.y) / 2,
+              (startAtom.z + endAtom.z) / 2
+            );
+  
+            mesh.lookAt(endAtom);
+            scene.add(mesh);
+            bonds.push(mesh);
+          // }
+          // );
         });
-        data.connectData.forEach((bond) => {
-          const startAtom = atoms.find(atom => atom.userData.serial === parseInt(bond[0]));
-          const endAtom = atoms.find(atom => atom.userData.serial === parseInt(bond[1]));
-
-          const distance = startAtom.position.distanceTo(endAtom.position);
-          const geometry = new CylinderGeometry(0.1, 0.1, distance, 32);
-          const mesh = new Mesh(geometry, bondMaterial);
-
-          mesh.position.set(
-            (startAtom.position.x + endAtom.position.x) / 2,
-            (startAtom.position.y + endAtom.position.y) / 2,
-            (startAtom.position.z + endAtom.position.z) / 2
-          );
-
-          mesh.lookAt(endAtom.position);
-          scene.add(mesh);
-          bonds.push(mesh);
-        });
-        // data.connectData.forEach((bond) => {
-        //   console.log('-----------------------------')
-        //   console.log(bond);
-        //   console.log('-----------------------------')
-        //   const startAtom = atoms[bond.startAtomIndex];
-        //   const endAtom = atoms[bond.endAtomIndex];
-        //   const distance = startAtom.position.distanceTo(endAtom.position);
-        //   const geometry = new CylinderGeometry(0.1, 0.1, distance, 32);
-        //   const mesh = new Mesh(geometry, bondMaterial);
-        //   mesh.position.set(
-        //    (startAtom.position.x + endAtom.position.x) / 2,
-        //     (startAtom.position.y + endAtom.position.y) / 2,
-        //     (startAtom.position.z + endAtom.position.z) / 2
-        //   );
-        //   mesh.lookAt(endAtom.position);
-        //   scene.add(mesh);
-        //   bonds.push(mesh);
-        // });
       }
-
-      // call render
       render();
-    })
+    });
   };
 
   return (
