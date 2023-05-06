f47105133d4e3c01bf87f85c513edb1e98781d7d
ahaddad98 <amine.haddadd@gmail.com>
2023-05-06 14:39:43 +0200

    change modal style

--- components/Modal.js
+++ components/Modal.js
@@ -2,6 +2,15 @@ import React from "react";
 import { Modal, View, Text, StyleSheet } from "react-native";
 import styled from "styled-components";
 
+const ModalContent = ({ title, name }) => {
+  return (
+    <View style={{ display: 'flex', flexDirection: 'row', fontSize: 20 }}>
+      <Text style={{ color: '#e97560', fontWeight: 'bold' }}>{title}</Text>
+      <Text style={{ marginLeft: 5 }}>{name}</Text>
+    </View>
+  )
+}
+
 const CustomModal = ({ data, visible, setVisible }) => {
   console.log(data);
   return (
@@ -9,13 +18,12 @@ const CustomModal = ({ data, visible, setVisible }) => {
       <View style={styles.modalContainer}>
         <View style={[styles.modalView, { backgroundColor: "white" }]}>
           <BottonStyle title="X" onPress={() => setVisible(false)}>
-            <Text>X</Text>
+            <Text style={{ color: '#ffff', fontWeight: 'bold', fontSize: 20 }}>X</Text>
           </BottonStyle>
-          <Text>{`Name: ${data?.name}`}</Text>
-          <Text>{`element: ${data?.element}`}</Text>
-          <Text>{`Phase: ${data?.phase}`}</Text>
-          <Text>{`Discoverd By: ${data?.discoverdBy}`}</Text>
-          {/* <Text>{name}</Text> */}
+          <ModalContent title={`Name: `} name={data?.name} />
+          <ModalContent title={`Element: `} name={data?.element} />
+          <ModalContent title={`Phase: `} name={data?.phase} />
+          <ModalContent title={`Discovered By: `} name={data?.discoverdBy} />
         </View>
       </View>
     </Modal>
@@ -31,10 +39,9 @@ const BottonStyle = styled.TouchableOpacity`
   display: flex;
   align-items: center;
   justify-content: center;
-  /* background: #E97560; */
   border-radius: 12px;
   align-self: flex-end;
-  /* color: #FFFF; */
+  background-color: #e97560;
 `;
 
 const styles = StyleSheet.create({
@@ -47,10 +54,74 @@ const styles = StyleSheet.create({
   modalView: {
     width: 250,
     height: 200,
-    // justifyContent: 'center',
-    alignItems: "center",
+    paddingLeft: 20,
     borderRadius: 10,
   },
 });
 
 export default CustomModal;
+
+// import React from "react";
+// import { Modal, View, Text, StyleSheet } from "react-native";
+// import styled from "styled-components";
+
+
+// const ModalContent = ({ title, name }) => {
+//   return <View style={{ display: 'flex', flexDirection: 'row', fontSize: 20 }}>
+//     <Text style={{ color: '#e97560' }}> {title}</Text>
+//     <Text>{name}</Text>
+//   </View>
+// }
+
+// const CustomModal = ({ data, visible, setVisible }) => {
+//   console.log(data);
+//   return (
+//     <Modal transparent visible={visible} animationType="slide">
+//       <View style={styles.modalContainer}>
+//         <View style={[styles.modalView, { backgroundColor: "white" }]}>
+//           <BottonStyle title="X" onPress={() => setVisible(false)}>
+//             <Text>X</Text>
+//           </BottonStyle>
+//           <ModalContent title={`Name: `} name={data?.name} />
+//           <ModalContent title={`element: `} name={data?.element} />
+//           <ModalContent title={`Phase: `} name={data?.phase} />
+//           <ModalContent title={`Discoverd By: `} name={data?.discoverdBy} />
+//         </View>
+//       </View>
+//     </Modal>
+//   );
+// };
+
+// const BottonStyle = styled.TouchableOpacity`
+//   width: 40px;
+//   height: 40px;
+//   margin-top: 10px;
+//   margin-bottom: 10px;
+//   margin-right: 10px;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   /* background: #E97560; */
+//   border-radius: 12px;
+//   align-self: flex-end;
+//   /* color: #FFFF; */
+// `;
+
+// const styles = StyleSheet.create({
+//   modalContainer: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "rgba(0, 0, 0, 0.5)",
+//   },
+//   modalView: {
+//     width: 250,
+//     height: 200,
+//     // justifyContent: 'center',
+//     // alignItems: "center",
+//     paddingLeft: 20,
+//     borderRadius: 10,
+//   },
+// });
+
+// export default CustomModal;
